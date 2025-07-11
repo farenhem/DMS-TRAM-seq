92583b55cb9370feb66f80e7b86a230b6410bbd3
Kelsey Farenhem <farenhem@wi.mit.edu>
2025-07-11 17:02:39 -0400

    Update getGenomeRatiosStrand_chr.sh

--- code/getGenomeRatiosStrand_chr.sh
+++ code/getGenomeRatiosStrand_chr.sh
@@ -8,6 +8,9 @@ pileuppath=../pileups
 sizes=../reference_annot/fasta_hg38/chromInfo.txt
 bamfileroot=$1
 
+mkdir -p ${jobpath}
+mkdir -p ${pileuppath}
+
 for chr in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY chrM
 do
 
