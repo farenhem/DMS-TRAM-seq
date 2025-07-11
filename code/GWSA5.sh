f686952550a9d927605660ae0d7f986c66083c15
Kelsey Farenhem <farenhem@wi.mit.edu>
2025-07-11 17:05:14 -0400

    Update GWSA5.sh

--- code/GWSA5.sh
+++ code/GWSA5.sh
@@ -5,7 +5,6 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16gb
 #SBATCH --partition=20
-#SBATCH --output /lab/jain_imaging/Kelsey/Sequencing/20210903_NovaSeq/ANALYSIS/jobs/GWSA5_%j.out
 
 # This script is a control script for genome-wide analysis of DMS-MaPseq
 # data in different conditions, starting with BAM files (i.e. mapped reads)
@@ -17,9 +16,8 @@
 # condition, as retained (i.e. normalized) sites must have passed the
 # threshold criterion in each control/DMS-treated sample pair.
 
-path=/lab/jain_imaging/Kelsey/Sequencing/20210903_NovaSeq
-jobpath=${path}/ANALYSIS/jobs
-bedGraphpath=${path}/ANALYSIS/bedGraph
+jobpath=../jobs
+bedGraphpath=../bedGraph
 
 # List experiments from non-stress conditions.
 DMS1=D1
